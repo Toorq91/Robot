14dacd0a356531059453b0d27d137c40bf2edc5e
Toorq91 <157486467+Toorq91@users.noreply.github.com>
2024-06-10 11:30:19 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using SimpleRobotLogic;
+Console.CursorVisible = false;
+var robot = new Robot.Robot();
+robot.Show();
+robot.TurnRight();
+Console.ReadKey(true);
+robot.GoBox();
+
+//robot.TurnRight();
+//robot.Show();
+//Console.ReadKey(true);
+//robot.Go();
+//robot.Show();
+//Console.ReadKey(true);
+//robot.Go();
+//robot.Show();
+//Console.ReadKey(true);
+//robot.Go();
+//Console.ReadKey(true);
---
+++ Robot.cs
@@ -0,0 +1,64 @@
+﻿using SimpleRobotLogic;
+
+namespace Robot
+{
+    internal class Robot
+    {
+        private SimpleRobot simpleRobot;
+
+        public Robot()
+        {
+            this.simpleRobot = new SimpleRobot();
+        }
+
+        public void GoBox()
+        {
+            GoThreeTimesAndTurnLeft();
+            GoThreeTimesAndTurnLeft();
+            GoThreeTimesAndTurnLeft();
+            GoThreeTimesAndTurnLeft();
+        }
+
+        private void GoThreeTimesAndTurnLeft()
+        {
+            simpleRobot.Go();
+            Show();
+            simpleRobot.Go();
+            Show();
+            simpleRobot.Go();
+            Show();
+            simpleRobot.TurnLeft();
+        }
+
+        public void Show()
+        {
+            simpleRobot.Show();
+        }
+
+        public void TurnLeft()
+        {
+            simpleRobot.TurnLeft();
+        }
+
+        public void Go()
+        {
+            simpleRobot.Go();
+        }
+
+        public void TurnRight()
+        {
+            simpleRobot.TurnLeft();
+            simpleRobot.TurnLeft();
+            simpleRobot.TurnLeft();
+        }
+
+        public void GoToWall()
+        {
+            while (simpleRobot.Go())
+            {
+                Show();
+            }
+        }
+        
+    }
+}
